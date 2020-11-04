6a8f1926abd5577c0690fe74fffa3965394c47fb
Jonathon Bray <jonathonbray@Jonathons-MacBook-Pro.local>
2020-11-04 16:15:02 +0000

    now gets image and sets t as the background

--- LandingPage.js
+++ LandingPage.js
@@ -1,5 +1,14 @@
 $(function(){
-    //$.get("https://api.nasa.gov/planetary/apod?api_key=eBUo0pnuAct7kPtwO1uHsAJysnqUX7H7fYvcL7UQ", function(data){
-    //    $("#body").css({"background-image": "url(" + data.url + ")"});
-    //});
+    var imageGenre = "animal";
+    let randPage = getRandomNumber(5);
+    var apiKey = "18981643-ee07b4d55bd89f8d3b9d18f9c";
+    var url = "https://pixabay.com/api/?key=" + apiKey + "&q=" + imageGenre + "&per_page=100" + "&page" + randPage;
+    $.get(url, function(data){
+        let randImg = getRandomNumber(100);
+        $("#body").css({"background-image": "url(" + data.hits[randImg].largeImageURL + ")"});
+    });
 })
+
+function getRandomNumber(max){
+    return Math.floor(Math.random() * Math.floor(max));
+}
