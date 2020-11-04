72a938d0f70815b66c64d647589033286588b790
Jonathon Bray <jonathonbray@Jonathons-MacBook-Pro.local>
2020-11-04 14:42:07 +0000

    srtarted styling

--- LandingPage.js
+++ LandingPage.js
@@ -1,5 +1,5 @@
 $(function(){
-    $.get("https://api.nasa.gov/planetary/apod?api_key=eBUo0pnuAct7kPtwO1uHsAJysnqUX7H7fYvcL7UQ", function(data){
-        $("#body").css({"background-image": "url(" + data.url + ")"});
-    });
+    //$.get("https://api.nasa.gov/planetary/apod?api_key=eBUo0pnuAct7kPtwO1uHsAJysnqUX7H7fYvcL7UQ", function(data){
+    //    $("#body").css({"background-image": "url(" + data.url + ")"});
+    //});
 })
