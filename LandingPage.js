6f581898e4de17b2903c8e5bcdfd472d19fe2205
Jonathon Bray <jonathonbray@Jonathons-MacBook-Pro.local>
2020-11-05 22:56:27 +0000

    Added weather image

--- LandingPage.js
+++ LandingPage.js
@@ -9,8 +9,10 @@ $(function(){
     //Call time function
     showTime();
     //Get weather
-    $.get("https://api.weatherapi.com/v1/current.json?key=4947a4d458c04be0811153820200511&q=auto:ip", function(weather){
-        console.log(weather.current.temp_c);
+    var city = "plymouth";
+    $.get("https://api.weatherapi.com/v1/current.json?key=4947a4d458c04be0811153820200511&q=" + city, function(weather){
+        console.log(getWeatherIcon(weather.current.condition.code));
+        $("#weatherImg").prop("src", getWeatherIcon(weather.current.condition.code));
     });
     //Get and set quote
     $.get("https://api.quotable.io/random", function(quotes){
@@ -107,4 +109,103 @@ function showTime(){
 
     setTimeout(showTime, 1000);
 }
-
+//Get weather icon
+function getWeatherIcon(code){
+    var time = new Date().getHours();
+    switch(code){
+        //Sunny or clear night
+        case 1000:
+            if(time >= 6 && time < 20){
+                return "https://img.icons8.com/ios/72/sun--v1.png";
+            }
+            else{
+                return "https://img.icons8.com/ios/72/bright-moon.png";
+            }
+        //Partly cloudy day or night
+        case 1003:
+            if(time >= 6 && time < 20){
+                return "https://img.icons8.com/ios/72/partly-cloudy-day.png";
+            }
+            else{
+                return "https://img.icons8.com/ios/72/partly-cloudy-night--v1.png";
+            }
+        //Cloudy
+        case 1006:
+        case 1009:
+            return "https://img.icons8.com/ios/72/cloud.png";
+        //Mist, haze or fog
+        case 1030:
+        case 1135:
+        case 1147:
+            if(time >= 6 && time < 20){
+                return "https://img.icons8.com/ios/72/fog-day.png";
+            }
+            else{
+                return "https://img.icons8.com/ios/72/fog-night.png";
+            }
+        //Patchy rain
+        case 1063:
+        case 1072:
+        case 1180:
+            return "https://img.icons8.com/ios/72/partly-cloudy-rain--v2.png";
+        //Light rain
+        case 1150:
+        case 1153:
+        case 1168:    
+        case 1180:
+        case 1183:
+        case 1198:
+        case 1240:
+            return "https://img.icons8.com/ios/72/light-rain.png";
+        //Moderate rain
+        case 1171:
+        case 1186:
+        case 1189:
+        case 1201:
+        case 1243:
+            return "https://img.icons8.com/ios/72/moderate-rain.png";
+        //Heavy rain
+        case 1192:
+        case 1195:
+            return "https://img.icons8.com/ios/72/heavy-rain.png";
+        //Torrential rain
+        case 1246:
+            return "https://img.icons8.com/ios/72/torrential-rain--v1.png";
+        //Light snow
+        case 1066:
+        case 1114:
+        case 1210:
+        case 1213:
+        case 1216:
+        case 1255:
+            return "https://img.icons8.com/ios/72/light-snow.png";
+        //Blizzard
+            case 1117:
+                return "https://img.icons8.com/ios/72/snow-storm--v1.png";
+        //Heavy snow
+        case 1219:
+        case 1222:
+        case 1225:
+        case 1258:
+            return "https://img.icons8.com/ios/72/snow.png";
+        //Sleet
+        case 1069:
+        case 1204:
+        case 1207:
+        case 1249:
+        case 1252:
+            return "https://img.icons8.com/ios/72/sleet.png";
+        //Stormy weather
+        case 1087:
+        case 1273:
+        case 1279:
+        case 1276:
+        case 1282:
+            return "https://img.icons8.com/ios/72/storm--v1.png";
+        //Hail
+        case 1237:
+        case 1261:
+        case 1264:
+            return "https://img.icons8.com/ios/72/hail.png";
+    }
+}
