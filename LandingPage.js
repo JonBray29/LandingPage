45399fdc57716ea688640738932e40e96e197403
Jonathon Bray <jonathonbray@Jonathons-MacBook-Pro.local>
2020-11-05 12:31:55 +0000

    Adds random quote to page and updates greeting

--- LandingPage.js
+++ LandingPage.js
@@ -1,55 +1,71 @@
 $(function(){
+    //Get background image dependant on time of day
     var apiKey = "18981643-ee07b4d55bd89f8d3b9d18f9c";
     var imgId = getIdForTime();
     var url = "https://pixabay.com/api/?key=" + apiKey + "&id=" + imgId;
     $.get(url, function(data){
         $("#body").css({"background-image": "url(" + data.hits[0].largeImageURL + ")"});
     });
+    //Get and set quote
+    $.get("https://api.quotable.io/random", function(quotes){
+        $("#quote").html(quotes.content + " - " + quotes.author);
+    });
+    //Set the greeting dependant on time of day
+    var greeting = getGreeting();
+    $("#greeting").html(greeting);
 })
-
+//Get image ID dependant on the time of day
 function getIdForTime(){
-    var id;
     switch(new Date().getHours()){
         case 2:
         case 3:
         case 4:
-            id = 2111811;
-            break;
+            return 2111811;
         case 5:
         case 6:
         case 7:
-            id = 3358870;
-            break;
+            return 3358870;
         case 8:
         case 9:
         case 10:
-            id = 2836301;
-            break;
+            return 2836301;
         case 11:
         case 12:
         case 13:
-            id = 3601004;
-            break;
+            return 3601004;
         case 14:
         case 15:
         case 16:
-            id = 3082832;
-            break;
+            return 3082832;
         case 17:
         case 18:
         case 19:
-            id = 1589616;
-            break;
+            return 1589616;
         case 20:
         case 21:
         case 22:
-            id = 1979546;
-            break;
+            return 1979546;
         case 23:
         case 24:
         case 1:
-            id = 3194001;
-            break;
+            return 3194001;
     }
-    return id;
 }
+//Get the greeting dependant on the time of day
+function getGreeting(){
+    let time = new Date().getHours();
+
+    if(time >= 5 && time < 12){
+        return "Good Morning";
+    }
+    else if(time >= 12 && time < 17){
+        return "Good afternoon";
+    }
+    else if(time >= 17 && time < 20){
+        return "Good evening";
+    }
+    else{
+        return "Good Night";
+    }
+}
+
--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # LandingPage
 Personal project: 
+  - Displays a background image dependant on the time of day, using Pixabay. https://pixabay.com/service/about/api/
+  - Quotable, an API that fetches a random quote. https://github.com/lukePeavey/quotable
+
+
   - Stores users name in local sotrage
   - Displays the time and date
-  - Uses NASAs APOD API for background image
   - Displays the weather
   - To do list
   - Settings to turn off certain features
